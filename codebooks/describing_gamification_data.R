5e8a08ebacc27710e9923194560b04a1a212aef4
Philipp Chapkovski <chapkovski@gmail.com>
2023-11-21 12:20:38 +0100

    adding  codebooks for data_prolific

--- codebooks/describing_gamification_data.R
+++ codebooks/describing_gamification_data.R
@@ -1,7 +1,7 @@
 # R code for making codebooks
 # 
 library(pacman)
-p_load(codebook, labelled, purrr)
+p_load(codebook, labelled, purrr, tidyverse, glue, dataMaid)
 
 # taking the sheet with var descriptions from google drive (or locally - later)
 p_load(googlesheets4)
@@ -92,3 +92,44 @@ varinfo<- description_data %>% filter(filename==fn)
 var_labels <- setNames(varinfo$description, varinfo$variable_name)
 makeCodebook(df, glue('{fn}'), reportTitle = fn, replace=T) 
 
+
+
+
+# NOW DATA_PROLIFIC FILES
+prolific_fn<-'/Users/chapkovski/Documents/gamification_investor_behavior/codebooks/codebook_data_prolific.csv'
+# Reading the first sheet
+description_data <- read_csv(prolific_fn)
+
+# attempt to build codebooks automatically
+description_data %>% group_by(filename) %>% group_split()->grouped_tibbles
+
+# we write file descripitons in csv separately for each file
+for (tibble in grouped_tibbles) {
+  # Perform operations on each tibble
+  fn<-tibble %>% dplyr::slice(1) %>% pull(filename)
+  print(fn)
+  tibble %>% select(variable_name, description) %>% write_csv(file=glue('codebook_{fn}'))
+}
+# change it to the right path
+path = '/Users/chapkovski/Documents/gamification_investor_behavior/data_prolific/'
+
+
+fn<-"markov_trade_flags.csv"  
+produce_codebook <- function(fn){
+  unnessary_columns = c('...1', 'participant.label', 'participant.code', 'body')
+  df<-read_csv(glue('{path}{fn}'), col_names=T) %>%  select(-any_of(unnessary_columns))
+  varinfo<- description_data %>% filter(filename==fn)%>% 
+    filter(variable_name%in%(df %>% names))
+  var_labels <- setNames(varinfo$description, varinfo$variable_name)
+  df<-df %>% set_variable_labels(.labels=var_labels)
+  makeCodebook(df, glue('{fn}'), reportTitle = fn, replace=T, openResult=F) 
+  
+}
+
+for (tibble in grouped_tibbles) {
+  # Perform operations on each tibble
+  fn<-tibble %>% dplyr::slice(1) %>% pull(filename)
+  print(fn)
+  produce_codebook(fn)
+}
+ 
